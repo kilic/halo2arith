628f7581ab8ca6e4408deff03e3f3de3a2929944
kilic <kiliconu@itu.edu.tr>
2022-01-04 01:03:33 +0300

    add four width gate

---
+++ src/lib.rs
@@ -0,0 +1,116 @@
+use halo2::plonk::Error;
+use halo2::{
+    arithmetic::FieldExt,
+    circuit::{Cell, Region},
+};
+use std::marker::PhantomData;
+use utils::decompose;
+
+pub mod main_gate;
+pub mod utils;
+
+pub trait Assigned<F: FieldExt> {
+    fn value(&self) -> Option<F>;
+    fn constrain_equal(&self, region: &mut Region<'_, F>, new_cell: Cell) -> Result<(), Error> {
+        region.constrain_equal(self.cell(), new_cell)
+    }
+    fn cell(&self) -> Cell;
+    fn decompose(&self, number_of_limbs: usize, bit_len: usize) -> Option<Vec<F>> {
+        self.value().map(|e| decompose(e, number_of_limbs, bit_len))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct AssignedCondition<F: FieldExt> {
+    bool_value: Option<bool>,
+    cell: Cell,
+    _marker: PhantomData<F>,
+}
+
+impl<F: FieldExt> From<AssignedValue<F>> for AssignedCondition<F> {
+    fn from(assigned: AssignedValue<F>) -> Self {
+        AssignedCondition::new(assigned.cell, assigned.value)
+    }
+}
+
+impl<F: FieldExt> AssignedCondition<F> {
+    fn new(cell: Cell, value: Option<F>) -> Self {
+        let bool_value = value.map(|value| if value == F::zero() { false } else { true });
+        AssignedCondition {
+            bool_value,
+            cell,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<F: FieldExt> Assigned<F> for AssignedCondition<F> {
+    fn value(&self) -> Option<F> {
+        self.bool_value
+            .map(|value| if value { F::one() } else { F::zero() })
+    }
+    fn cell(&self) -> Cell {
+        self.cell
+    }
+}
+
+type AssignedBit<F> = AssignedCondition<F>;
+
+#[derive(Debug, Clone)]
+pub struct AssignedValue<F: FieldExt> {
+    pub value: Option<F>,
+    cell: Cell,
+}
+
+impl<F: FieldExt> From<AssignedCondition<F>> for AssignedValue<F> {
+    fn from(cond: AssignedCondition<F>) -> Self {
+        AssignedValue {
+            value: (&cond).value(),
+            cell: cond.cell,
+        }
+    }
+}
+
+impl<F: FieldExt> Assigned<F> for AssignedValue<F> {
+    fn value(&self) -> Option<F> {
+        self.value
+    }
+    fn cell(&self) -> Cell {
+        self.cell
+    }
+}
+
+impl<F: FieldExt> AssignedValue<F> {
+    fn new(cell: Cell, value: Option<F>) -> Self {
+        AssignedValue { value, cell }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct UnassignedValue<F: FieldExt> {
+    pub value: Option<F>,
+}
+
+impl<F: FieldExt> From<Option<F>> for UnassignedValue<F> {
+    fn from(value: Option<F>) -> Self {
+        UnassignedValue { value }
+    }
+}
+
+impl<F: FieldExt> UnassignedValue<F> {
+    pub fn new(value: Option<F>) -> Self {
+        UnassignedValue { value }
+    }
+
+    pub fn value(&self) -> Result<F, Error> {
+        Ok(self.value.clone().ok_or(Error::Synthesis)?)
+    }
+
+    pub fn decompose(&self, number_of_limbs: usize, bit_len: usize) -> Option<Vec<F>> {
+        self.value.map(|e| decompose(e, number_of_limbs, bit_len))
+    }
+
+    pub fn assign(&self, cell: Cell) -> AssignedValue<F> {
+        AssignedValue::new(cell, self.value)
+    }
+}
---
+++ src/utils.rs
@@ -0,0 +1,29 @@
+use halo2::arithmetic::FieldExt;
+use num_bigint::BigUint as big_uint;
+use std::ops::Shl;
+
+pub fn big_to_fe<F: FieldExt>(e: big_uint) -> F {
+    F::from_str_vartime(&e.to_str_radix(10)[..]).unwrap()
+}
+
+pub fn fe_to_big<F: FieldExt>(fe: F) -> big_uint {
+    big_uint::from_bytes_le(&fe.to_bytes()[..])
+}
+
+pub fn decompose<F: FieldExt>(e: F, number_of_limbs: usize, bit_len: usize) -> Vec<F> {
+    decompose_big(fe_to_big(e), number_of_limbs, bit_len)
+}
+
+pub fn decompose_big<F: FieldExt>(e: big_uint, number_of_limbs: usize, bit_len: usize) -> Vec<F> {
+    let mut e = e;
+    let mask = big_uint::from(1usize).shl(bit_len) - 1usize;
+    let limbs: Vec<F> = (0..number_of_limbs)
+        .map(|_| {
+            let limb = mask.clone() & e.clone();
+            e = e.clone() >> bit_len;
+            big_to_fe(limb)
+        })
+        .collect();
+
+    limbs
+}
